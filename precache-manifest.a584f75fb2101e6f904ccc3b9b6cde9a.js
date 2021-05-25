4c46db3a49891b7dfd0845b0bd7ce2d43166b195
Ruslana Podshyvalova <lanataylorpersonal@gmail.com>
2021-05-25 19:31:10 +0300

    Updates

---
+++ precache-manifest.a584f75fb2101e6f904ccc3b9b6cde9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0173d6741ac57490748fdeea0527bc07",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6a11c09438877c4d38af",
+    "url": "./static/css/main.a4c23238.chunk.css"
+  },
+  {
+    "revision": "642c4c1f1e0aa508b7f8",
+    "url": "./static/js/2.982a7a1c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.982a7a1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a11c09438877c4d38af",
+    "url": "./static/js/main.acf8192b.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
